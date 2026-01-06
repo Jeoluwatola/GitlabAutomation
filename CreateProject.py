e238a8f147332283a5927c06e079c9e53de87204
jeoluwatola <joluwatola@Oluwatolas-MacBook-Pro.local>
2026-01-06 11:49:30 +0100

    updatecodestructure

--- CreateProject.py
+++ CreateProject.py
@@ -1,120 +1,205 @@
 import subprocess
 import json
+import requests
 
-GROUPID = 'GROUP_ID'
-API_token = 'TOKEN'
-projectname = 'Test-Project3'
-developers = ['Developer1', 'Developer2']
-maintainers = ['Maintainer1', 'JMaintainer2']
-subgroups = []
+ORGAdminID = 'ID'
+projectname = 'ORG-ops-starter'
+developers = []
+maintainers = []
+allowed_to_merge =[]
+GITLAB_API_TOKEN = 'TOKEN'
+BASE_URL = "https://gitlab.com/api/v4"
 
-#curl function
-def curl(command):
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    stdout, stderr = process.communicate()
+get_headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {GITLAB_API_TOKEN}"
+}
 
-    if process.returncode == 0:
-        return stdout
-    else:
-        return stderr
+post_headers = {
+    "Authorization": f"Bearer {GITLAB_API_TOKEN}"
+}
 
 
 #create project
-createproject = f'curl -X POST  -H "Authorization: Bearer {API_token}" -d "name={projectname}&namespace_id={GROUPID}&initialize_with_readme=true&default_branch=staging" --url "https://gitlab.com/api/v4/projects/"'
-page = curl(createproject)
-print(page)
-data = json.loads(page)
+createproject_params = {
+    "name" : projectname,
+    "namespace_id" : ORGAdminID,
+    "initialize_with_readme" : "true",
+    "default_branch" : "staging"
+}
+resp = requests.post(f"{BASE_URL}/projects/", headers=post_headers, params=createproject_params)
+data = resp.json()
+print(data)
 projectid = data.get("id", [])
 print(projectid)
 print(f'{projectname} was created with id {projectid}')
 
+
+
 #add approval rule
-addapprovalrule = f'curl -X POST  -H "Authorization: Bearer {API_token}" -d "name=approval&approvals_required=1&applies_to_all_protected_branches=true&rule_type=any_approver" --url "https://gitlab.com/api/v4/projects/{projectid}/approval_rules"'
-page = curl(addapprovalrule)
-print(page)
+addapprovalrule_params = {
+    "name" : 'approval',
+    "approvals_required" : '1',
+    "applies_to_all_protected_branches" : "true"
+}
+resp = requests.post(f"{BASE_URL}/projects/{projectid}/approval_rules", headers=post_headers, params=addapprovalrule_params)
+data = resp.json()
+print(data)
+
+
 
 #create main branch
-createbranch = f'curl -X POST  -H "Authorization: Bearer {API_token}" -d "branch=main&ref=staging" --url "https://gitlab.com/api/v4/projects/{projectid}/repository/branches"'
-page = curl(createbranch)
-print(page)
+createbranch_params = {
+    "branch" : "main",
+    "ref" : "staging",
+    "applies_to_all_protected_branches" : "true"
+}
+resp = requests.post(f"{BASE_URL}/projects/{projectid}/repository/branches", headers=post_headers, params=createbranch_params)
+data = resp.json()
+print(data)
 print('Main branch created')
 
-
-#protect main branch
-protectbranch = f'curl -X POST  -H "Authorization: Bearer {API_token}" -d "name=main" --url "https://gitlab.com/api/v4/projects/{projectid}/protected_branches"'
-page = curl(protectbranch)
-print(page)
-print('Main branch protected')
 
 
 #create subgroup
 subgroupname = f'{projectname}_Subgroup'
 subgrouppath = f'{subgroupname}'
-createsubgroup = f'curl -X POST -H "Authorization: Bearer {API_token}" -d "path={subgrouppath}&name={subgroupname}&parent_id={GROUPID}" --url "https://gitlab.com/api/v4/groups/"'
-page = curl(createsubgroup)
-data = json.loads(page)
-print (page)
+createsubgroup_params = {
+    "path" : f'{subgrouppath}',
+    "name" : f'{subgroupname}',
+    "parent_id" : f"{ORGAdminID}"
+}
+resp = requests.post(f"{BASE_URL}/groups/", headers=post_headers, params=createsubgroup_params)
+data = resp.json()
+print(data)
 subgroupid = data.get("id", [])
 print(f'{subgroupname} was created with id {subgroupid}')
 
-#Get userids of default maintainers of Subgroup
-getSubgroupmembers = f'curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer {API_token}" https://gitlab.com/api/v4/groups/{subgroupid}/members/all'
-page = curl(getSubgroupmembers)
-subgroupmembers = json.loads(page)
-print(subgroupmembers)
-userids = [item["id"] for item in subgroupmembers if item["access_level"] == 40]
-print(userids)
 
-#Make default maintainers owners of subgroup
-for i in userids:
-    username = next((item["name"] for item in subgroupmembers if item["id"] == i), None)
-    addUsertoSubgroup = f'curl -X POST -H "Authorization: Bearer {API_token}" -d "user_id={i}&access_level=50" https://gitlab.com/api/v4/groups/{subgroupid}/members/'
-    page = curl(addUsertoSubgroup)
-    print (page)
-    print (username + " is now an owner of the Subgroup")
 
 #add subgroup to project
-addSubgrouptoProject = f'curl -X POST -H "Authorization: Bearer {API_token}" -d "group_id={subgroupid}&group_access=40" https://gitlab.com/api/v4/projects/{projectid}/share'
-page = curl(addSubgrouptoProject)
-print (page)
+addsubgroup_params = {
+    "group_id" : f"{subgroupid}",
+    "group_access" : "40"
+}
+resp = requests.post(f"{BASE_URL}/projects/{projectid}/share", headers=post_headers, params=addsubgroup_params)
+data = resp.json()
+print(data)
 print ("Project has been shared with the subgroup")
 
 
-#Get memebers of group
-allmembers = json.loads('[]')
-for i in range(1,5,1):
-    getmembers = f'curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer {API_token}" https://gitlab.com/api/v4/groups/GROUP/members/?page={i}'
-    page = curl(getmembers)
-    a = json.loads(page)
-    allmembers = allmembers + a 
+#add devops to project
+adddevops_params = {
+    "group_id" : "id",
+    "group_access" : "40"
+}
+resp = requests.post(f"{BASE_URL}/projects/{projectid}/share", headers=post_headers, params=adddevops_params)
+data = resp.json()
+print(data)
+print ("Project has been shared with the Devops Team")
+
+
+#Get members of ORGAdmin group
+def get_all_members():
+    all_members = []
+    page = 1
+    while True:
+        resp = requests.get(f"{BASE_URL}/groups/ORGAdmin/members", headers=get_headers, params={"page": page})
+
+        if resp.status_code != 200:
+            print(f"Error fetching members: {resp.status_code} - {resp.text}")
+            break
+
+        data = resp.json()
+        if not data:
+            break
+
+        all_members.extend(data)
+        page += 1
+
+    return sorted(all_members, key=lambda x: x['name'].lower())
+
+allmembers = get_all_members()
 
 #Retrieve userids for developers
 developerids = []
 for i in developers:
     userid = next((item["id"] for item in allmembers if item["name"] == i), None)
     developerids.append(userid)
-print(developerids)
 
 #Retrieve userids for maintainers 
 maintainerids = []
 for i in maintainers:
     userid = next((item["id"] for item in allmembers if item["name"] == i), None)
     maintainerids.append(userid)
-print(maintainerids)
+
+#Retrieve userids for those allowed to merge
+allowed_to_mergeids = []
+for i in allowed_to_merge:
+    userid = next((item["id"] for item in allmembers if item["name"] == i), None)
+    allowed_to_mergeids.append(userid)
 
 
 #add users to subgroup
 for i in developerids:
+    adduser_params = {
+    "user_id" : f"{i}",
+    "access_level" : "30"
+    }
     username = next((item["name"] for item in allmembers if item["id"] == i), None)
-    addUsertoSubgroup = f'curl -X POST -H "Authorization: Bearer {API_token}" -d "user_id={i}&access_level=30" https://gitlab.com/api/v4/groups/{subgroupid}/members'
-    page = curl(addUsertoSubgroup)
-    print(page)
+    resp = requests.post(f"{BASE_URL}/groups/{subgroupid}/members", headers=post_headers, params=adduser_params)
+    data = resp.json()
+    print(data)
     print(username + ' was added as a Developer')
 
 for i in maintainerids:
+    adduser_params = {
+    "user_id" : f"{i}",
+    "access_level" : "40"
+    }
     username = next((item["name"] for item in allmembers if item["id"] == i), None)
-    addUsertoSubgroup = f'curl -X POST -H "Authorization: Bearer {API_token}" -d "user_id={i}&access_level=40" https://gitlab.com/api/v4/groups/{subgroupid}/members'
-    page = curl(addUsertoSubgroup)
-    print(page)
+    resp = requests.post(f"{BASE_URL}/groups/{subgroupid}/members", headers=post_headers, params=adduser_params)
+    data = resp.json()
+    print(data)
     print(username + ' was added as a Maintainer')
     
+
+
+#protect main branch
+protectbranch_params = {
+    "name" : "main",
+    "push_access_level" : "40",
+    "merge_access_level" : "40"
+    }
+resp = requests.post(f"{BASE_URL}/projects/{projectid}/protected_branches", headers=post_headers, params=protectbranch_params)
+data = resp.json()
+print(data)
+print('Main branch protected')
+
+
+
+#Allow merge rights on staging branch
+for i in allowed_to_mergeids:
+    allowmerge_params = {
+        "allowed_to_merge%5B%5D%5Buser_id%5D" : f"{i}",
+        "allowed_to_push%5B%5D%5Buser_id%5D" : f"{i}"
+        }
+    username = next((item["name"] for item in allmembers if item["id"] == i), None)
+    resp = requests.patch(f"{BASE_URL}/projects/{projectid}/protected_branches/staging", headers=post_headers, params=allowmerge_params)
+    data = resp.json()
+    print(data)
+    print(username + ' was granted access to merge on staging')
+
+
+
+#Allow merge rights on main branch
+for i in allowed_to_mergeids:
+    allowmerge_params = {
+        "allowed_to_merge%5B%5D%5Buser_id%5D" : f"{i}",
+        "allowed_to_push%5B%5D%5Buser_id%5D" : f"{i}"
+        }
+    username = next((item["name"] for item in allmembers if item["id"] == i), None)
+    resp = requests.patch(f"{BASE_URL}/projects/{projectid}/protected_branches/main", headers=post_headers, params=allowmerge_params)
+    data = resp.json()
+    print(data)
+    print(username + ' was granted access to merge on main')
